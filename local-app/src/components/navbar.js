625d7cea2adbb088c90c67c86d5ff8501760fce3
hijifiji <hijifiji@hotmail.com>
2022-06-24 14:32:07 -0700

    Nav bar added

--- local-app/src/components/navbar.js
+++ local-app/src/components/navbar.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icon from '../img/GearShare.png'
+import '../styles/navbar.css';
 
 export default function Navbar(props) {
     return (
-        <div id='navbar'>
-            <h1>This is Navbar</h1>
-            <Link to='/'>
-                <button>
-                    {/* event handler to decide if login or
-                            sign up*/}
-                    LOGO
-                </button>
-            </Link>
-            <Link to='/loginsignup'>
-                <button>
-                    {/* event handler to decide if login or
-                            sign up*/}
-                    Log in or Sign Up
-                </button>
-            </Link>
-            <Link to='/rent'>
-                <button>
-                    Rent
-                </button>
-            </Link>
-            <Link to='/post'>
-                <button>
-                    Post
-                </button>
-            </Link>
-        </div>
+        <nav id="navbar">
+            <img src={icon} alt="Logo"/>                
+            <ul>
+                <Link to="/">Browse</Link>
+                <Link to="/">About</Link> 
+                <Link to="/">Reviews</Link>
+                <Link to="/">Sign Up</Link>                
+            </ul>
+            <ul>
+                <Link to="/" className="nav-login">Login</Link>
+            </ul>
+        </nav>
     );
 }
