87e3c0b3eeaaefcf809ebf97c91a3474ba6c0c02
Jerome <jeromejjcho@gmail.com>
2022-06-25 14:00:10 -0700

    Add modal to rentpage

---
+++ local-app/src/components/modal.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const title = this.props.title;
+        const dates = this.props.dates;
+        const rate = this.props.rate;
+        const description = this.props.description;
+        const contact = this.props.contact;
+
+            if (this.props.isDisplayed) {
+                return (
+                    <div id='overlay'>
+                        <div id='pop-up'>
+                            <div id='close-button' onClick={this.props.onClose}>
+                                <div className='crossbar' id='left-crossbar'>
+                                </div>
+                                <div className='crossbar' id='right-crossbar'>
+                                </div>
+                            </div>
+                            <h1 className='main-text'>{title}</h1>
+                            <hr />
+                            <p className='main-text' id='dates'>
+                                {dates}
+                            </p>
+                            <p className='main-text' id='rate'>
+                                {rate}
+                            </p>
+                            <p className='main-text' id='description'>
+                                {description}
+                            </p>
+                            <p className='main-text' id='contact'>
+                                {contact}
+                            </p>
+                        </div>
+                    </div>
+                )
+            } else {
+                return null;
+            }
+   }
+}
--- local-app/src/components/rentpage.js
+++ local-app/src/components/rentpage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import axios from 'axios'
+import uniqid from 'uniqid';
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './navbar';
-import uniqid from 'uniqid';
+import Modal from './modal';
 
 export default function RentPage() {
     const [gears, setGears] = useState([]);
@@ -32,16 +33,56 @@ export default function RentPage() {
         fetchGearList();
     },);
 
+    const [isModalDisplayed, setModalDisplayed] = useState(false);
+    const [currentTitle, setCurrentTitle] = useState('');
+    const [currentDates, setCurrentDates] = useState('');
+    const [currentRate, setCurrentRate] = useState('');
+    const [currentDescription, setCurrentDescription] = useState('');
+    const [currentContact, setCurrentContact] = useState('');
+
+    const handleClose = () => {
+        setModalDisplayed(false);
+    }
+
+    const handleClick = (e) => {
+        const currentGear = lookupGear(e.target.firstChild.textContent);
+        setCurrentTitle(currentGear.title);
+        setCurrentDates(currentGear.dates);
+        setCurrentRate(currentGear.rate);
+        setCurrentDescription(currentGear.body);
+        setCurrentContact(currentGear.contact);
+        setModalDisplayed(true);
+    }
+
+    const lookupGear = (title) => {
+        let currentGear;
+        gears.forEach(obj => {
+            if (obj.title === title) {
+                currentGear = Object.assign({}, obj);
+            }
+        });
+        return currentGear;
+    }
+
     return (
         <div id='rent-page' className='section'>
             <Navbar />
+            <Modal 
+            title={currentTitle}
+            dates={currentDates}
+            rate={currentRate}
+            description={currentDescription}
+            contact={currentContact}
+            onClose={handleClose}
+            isDisplayed={isModalDisplayed}
+            />
             <div id='rent-container'>
                 <h2 className='main-text'>Rent Gear</h2>
                 <div id='items-container'>
                     {gears.map(                  
                         (gear) => {
                             return (                            
-                            <div className='item' key={uniqid()}>
+                            <div className='item' key={uniqid()} onClick={handleClick}>
                                 <p className='item-title' className='main-text'>
                                     {gear.title}
                                 </p>
