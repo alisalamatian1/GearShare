b72cae8e9abfb003612fbad3afe91328798424a9
Jerome <jeromejjcho@gmail.com>
2022-06-25 14:46:00 -0700

    Add controlled components for  signinpage

--- local-app/src/components/signuppage.js
+++ local-app/src/components/signuppage.js
@@ -7,6 +7,28 @@ import { Link } from 'react-router-dom';
 export default class SignupPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        // this.state.email
+        // this.state.password
+    }
+
+    handleEmailChange(e) {
+        this.setState({email: e.target.value});
+    }
+
+    handlePasswordChange(e) {
+        this.setState({password: e.target.value});
     }
 
     render() {
@@ -16,14 +38,14 @@ export default class SignupPage extends React.Component {
                 <h2 className='main-text'>Sign Up</h2>
                 <p className='main-text'>One account. All the world's gear.</p>
                 <div id='form-container'>
-                    <form id='loginsignup-form'>
+                    <form id='loginsignup-form' onSubmit={this.handleSubmit}>
                         <label for='email'>
                             Email: <br />
-                            <input id='email' />
+                            <input id='email' value={this.state.email} onChange={this.handleEmailChange}/>
                         </label>
                         <label for='password'>
                             Password: <br />
-                            <input id='password' />
+                            <input id='password' value={this.state.password} onChange={this.handlePasswordChange}/>
                         </label>
                         <div className='signup-buttons'>
                             <button className='sign-up-button'>
