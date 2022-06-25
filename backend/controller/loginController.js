c350fbec8799d5710725f74605d2b5fa8fd47b41
Ali Salamatian <alisalamatian1@gmail.com>
2022-06-25 14:43:45 -0700

    adding the sign up logic

--- backend/controller/loginController.js
+++ backend/controller/loginController.js
@@ -3,6 +3,32 @@
 const Login = require("../model/loginSchema");
 const ErrorResponse = require('../util/errorResponse');
 
+exports.signUp = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorResponse("Please provide both email and password"), 400);
+    } else if (password.length < 6) {
+        return next(new ErrorResponse("Your password has to be at least 6 characters"), 400);
+    } 
+
+    try {
+        const signUp = await Login.create({
+            email, password
+        })
+        
+        res.status(201).json({
+            success: true,
+            message: "user created"
+        })
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            error: err.message
+        })
+    }
+}
+
 exports.login = async (req, res, next) => {
     const {email, password} = req.body;
 
@@ -13,14 +39,18 @@ exports.login = async (req, res, next) => {
     } 
 
     try {
-        const login = await Login.create({
-            email, password
-        })
-        
-        res.status(201).json({
-            success: true,
-            message: "user created"
-        })
+        const login = await Login.findOne({email: email})
+        if (login == null) {
+            res.status(404).json({
+                success: false,
+                message: "user not found"
+            })
+        } else {
+            res.status(201).json({
+                success: true,
+                message: "user found"
+            })
+        }
     } catch(err) {
         res.status(500).json({
             success: false,
--- backend/routing/login.js
+++ backend/routing/login.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const login = require("../controller/loginController");
+const {login, signUp} = require("../controller/loginController");
 
 router.route("/login").post(login);
+router.route("/signUp").post(signUp);
 
 module.exports = router;
