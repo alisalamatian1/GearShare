09a8b775d23757791dc5d83b8e746d6acd52cf6e
Ali Salamatian <alisalamatian1@gmail.com>
2022-07-03 21:11:29 -0700

    adding the needed files for deployment on heroku

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,8 @@ app.use('/ls/', require('./routing/login'))
 
 const port = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../frontend/build'))
+}
+
 const server = app.listen(port, () => console.log(`Server running on port ${port}`));
